67bbdf01b80d6aab037a1feea64cc6e7b11d1dca
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2023-01-03 09:02:43 +0530

    Ecommerce Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import SignUp from './components/SignUp';
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
 
 function App() {
   return (
-    <SignUp/>
-  );
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="SignIn" element={ <SignIn/> } />
+        <Route path="SignUp" element={ <SignUp/> } />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <link rel='stylesheet' href='css/Home.css'></link>
+
+                <meta name='viewport' content='width=device-width,initial-scale=1, shrink-to-fit=no' />
+
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                    integrity="anonymous"
+                    crossOrigin="anonymous" />
+
+                <nav>
+                    <ul className="nav-flex-row">
+                        <li className="nav-item">
+                            <a href="#about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="Category">Category</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="SignUp">Sign Up</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="SignIn">SignIn</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="AdminHome">Admin Home</a>
+                        </li>
+
+                    </ul>
+                </nav>
+                <section className="section-intro">
+                    <header>
+                        <h1 style={{ color: "Purple" }}> Welcome To Online Shopping</h1>
+                    </header>
+                </section>
+            </div>
+        )
+    }
+}
